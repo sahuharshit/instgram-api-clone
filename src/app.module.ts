3cc004481bca40768d193836ba8dc0447697f47f
sahuharshit <sahu_harshit@yahoo.in>
2024-05-13 18:30:11 +0530

    feat: added TypeORM for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
+
+const TYPE_ORM_IMPORT = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  logging: true,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_DATABASE || 'instagram',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true' || true,
+});
 
 @Module({
-  imports: [],
+  imports: [TYPE_ORM_IMPORT],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  async onModuleInit() {
+    try {
+      console.log('Database connected successfully!');
+    } catch (error) {
+      console.error('Database connection failed:', error);
+    }
+  }
+}
